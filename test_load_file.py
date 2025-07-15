4fcba2603cd2765b5860f34d3d4d4ff97c480293
Sirichandana Godavarthi <69519314+chandana-godavarthi@users.noreply.github.com>
2025-07-15 19:22:17 +0530

    Update test_load_file.py

--- test_load_file.py
+++ test_load_file.py
@@ -181,3 +181,32 @@ def test_load_file_gz_file(mock_env):
         )
 
         assert result == "Success"
+def test_load_file_csv_file(mock_env):
+    with patch("common.read_query_from_postgres") as mock_postgres, \
+         *common_patches:
+
+        mock_spark, mock_dbutils, mock_read_parquet, mock_df = mock_env
+        mock_postgres.return_value.select.return_value.distinct.return_value.collect.return_value = []
+
+        mock_dbutils.fs.ls.return_value = [
+            MagicMock(name="file1.csv", path="/mnt/tp-source-data/WORK/file1.csv")
+        ]
+
+        result = common.load_file(
+            file_type="prod",
+            RUN_ID="file1",
+            CNTRT_ID="CONTRACT",
+            STEP_FILE_PATTERN="file_%.csv",
+            vendor_pattern="vendor",
+            notebook_name="test_notebook",
+            delimiter=",",
+            dbutils=mock_dbutils,
+            postgres_schema="schema",
+            spark=mock_spark,
+            refDBjdbcURL="url",
+            refDBname="dbname",
+            refDBuser="user",
+            refDBpwd="pwd"
+        )
+
+        assert result == "Success"
